f3b0c36a177e1487074c1835b4bc793a098ee2fe
Linda Nichols <lynnaloo@gmail.com>
2015-04-22 09:10:37 -0400

    Added logging for generated code

--- generators/javascript/drone.js
+++ generators/javascript/drone.js
@@ -11,6 +11,7 @@ Blockly.JavaScript['take_off'] = function (block) {
     code += 'drone.startPing();\n';
     code += 'drone.flatTrim();\n';
     code += 'drone.takeOff();\n';
+    code += 'console.log("Drone is taking off")\n';
     code += 'drone.flatTrim();\n';
   return code;
 };
